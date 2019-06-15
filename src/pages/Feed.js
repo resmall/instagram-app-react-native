47d9dbb0568c41db71fd6b865e9a78c63506810a
Tiago R. Lammers <tiago.lammers@deliverymuch.com.br>
2019-06-15 14:33:19 -0300

    Initial

---
+++ src/pages/Feed.js
@@ -0,0 +1,133 @@
+import React, { Component } from 'react';
+
+import { View, Image, TouchableOpacity, StyleSheet, FlatList, Text } from 'react-native';
+
+import api from '../services/api';
+
+import camera from '../assets/camera.png';
+import more from '../assets/more.png';
+import like from '../assets/like.png';
+import comment from '../assets/comment.png';
+import send from '../assets/send.png';
+
+export default class Feed extends Component {
+    static navigationOptions = ({ navigation }) => ({
+        headerRight: (
+            <TouchableOpacity style={{marginRight: 20}} onPress={() => navigation.navigate('New')}>
+                <Image source={camera} />
+            </TouchableOpacity>
+        )
+    });
+
+    state = {
+        feed: []
+    };
+
+    async componentDidMount() {
+        //this.registerToSocket();
+
+        const response = await api.get('posts');
+        console.log(response)
+
+        this.setState({ feed: response.data });
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <FlatList
+                    data={this.state.feed}
+                    keyExtractor={post => post._id}
+                    renderItem={({item}) => (
+                        <View style={styles.feedItem}>
+                            <View style={styles.feedItemHeader}>
+                                <View style={styles.userInfo}>
+                                    <Text style={styles.name}>{item.author}</Text>
+                                    <Text style={styles.place}>{item.place}</Text>
+                                </View>
+                                <Image source={more} />
+                            </View>
+
+                            <Image style={styles.feedImage} source={{uri: `http://10.1.1.5:3333/files/${item.image}`}} />
+
+                            <View style={styles.feedItemFooter}>
+                                <View style={styles.actions}>
+                                    <TouchableOpacity style={styles.action} onPress={() => {}}>
+                                        <Image source={like} />
+                                    </TouchableOpacity>
+                                    <TouchableOpacity style={styles.action} onPress={() => {}}>
+                                        <Image source={comment} />
+                                    </TouchableOpacity>
+                                    <TouchableOpacity style={styles.action} onPress={() => {}}>
+                                        <Image source={send} />
+                                    </TouchableOpacity>
+                                </View>
+                            </View>
+                        </View>
+                    )}
+                />
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+
+    feedItem: {
+        marginTop: 20,
+    },
+
+    feedItemHeader: {
+        // paddingHorizontal: 1,
+        flexDirection: 'row',
+        justifyContent:'space-between',
+        alignItems: 'center'
+    },
+
+    name: {
+        fontSize: 14,
+        color: '#000'
+    },
+
+    place: {
+        fontSize: 12,
+        color:'#666',
+        marginTop: 2,
+    },
+
+    feedImage: {
+        width: '100%',
+        height: 400,
+        marginVertical: 15,
+    },
+
+    feedItem: {
+        paddingHorizontal: 15
+    },
+
+    actions: {
+        flexDirection: 'row',
+    },
+
+    action: {
+        marginRight: 8,
+    },
+
+    likes: {
+        marginTop: 15,
+        fontWeight:'bold',
+        color: '#000',
+    },
+
+    description: {
+        lineHeight: 18,
+        color: '#000',
+    },
+
+    hashtags: {
+        color: '#7159c1',
+    },
+});
